7cd8bafb3e3a7819a9be754f7b50764480df0ec8
Leechael Yim <yanleech@gmail.com>
2024-08-21 11:59:13 +0800

    update secret example

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,14 @@ import { handle } from '@phala/wapo-env/guest'
 const app = new Hono()
 
 app.get('/', (c) => {
+  let vault: Record<string, string> = {}
+  try {
+    vault = JSON.parse(process.env.secret || '')
+  } catch (e) {
+  }
+  if (vault.foo) {
+    return c.json({ message: vault.foo })
+  }
   return c.json({ message: 'Hello World' })
 })
 
