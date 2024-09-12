064cdba84f47002ac937e636aad17458b7f62629
Leechael Yim <yanleech@gmail.com>
2024-09-12 20:39:41 +0800

    start local wapo testnet with docker

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,19 @@
+import '@phala/wapo-env'
 import { Hono } from 'hono/tiny'
 import { handle } from '@phala/wapo-env/guest'
 
 const app = new Hono()
 
 app.get('/', (c) => {
-  let vault: Record<string, string> = {}
-  try {
-    vault = JSON.parse(process.env.secret || '')
-  } catch (e) {
-  }
-  if (vault.foo) {
-    return c.json({ message: vault.foo })
-  }
-  return c.json({ message: 'Hello World' })
+  const derivedKey = Wapo.deriveSecret("SALTY");
+  const hex = Buffer.from(derivedKey).toString('hex');
+  return c.json({ message: `${hex}` })
+})
+
+app.post('/', async (c) => {
+  const data = await c.req.json()
+  console.log('user payload in JSON:', data)
+  return c.json(data)
 })
 
 export default handle(app)
